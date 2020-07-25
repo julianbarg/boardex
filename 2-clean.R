d65c80ac561c9071438366476b8c70cf54d10ce9
julianbarg <barg.julian@gmail.com>
2020-07-25 16:26:46 -0400

    Extended cleaning to address issue of duplicate observations.

--- 2-clean.R
+++ 2-clean.R
@@ -3,17 +3,36 @@
 library(arrow)
 library(tidyverse)
 
-#  Clean the preprocessed file--mostly removing 
-temp <- read_feather("data/na_summary_preprocessed.feather")
+# Clean the na_summary_preprocessed file--mostly removing.
+df <- read_feather("data/na_summary_preprocessed.feather")
 
-# See data_validation for justification
-temp<- filter(temp,isin != "US30224P2002")
-temp <- temp %>%
-    mutate(year = lubridate::year(annualreportdate)) %>%
-    group_by(companyid, year) %>%
-    mutate(n = n_distinct(annualreportdate)) %>%
-    filter(n == 1) %>%
-    ungroup() %>%
-    select(-year, - n)
+# See data_validation for justification.
+df <- filter(df,isin != "US30224P2002")
 
-write_feather(temp, "data/na_summary_cleaned.feather")
+# There are some duplicates. we retain only unique observations
+df <- unique(df)
+
+# In some cases, there are multiple diverging observations for the same person.
+# We address that by first filling NAs and then removing remaining offending observations.
+df$nas <- rowSums(is.na(df))
+df <- df %>%
+    group_by(isin, annualreportdate, directorid) %>%
+    arrange(nas) %>%
+    fill(totalcompensation, eqlinkremratio) %>%
+    select(-nas) %>%
+    unique()
+
+inconsistencies <- df %>%
+    group_by(isin, annualreportdate, directorid) %>%
+    distinct() %>%
+    summarize(n = n()) %>%
+    filter(n>1) %>%
+    select(-directorid)
+inconsistencies$inconsistent <- TRUE
+
+df <- df %>%
+    left_join(inconsistencies, by = c("isin", "annualreportdate")) %>%
+    filter(!inconsistent %in% c(TRUE)) %>%
+    select(-inconsistent)
+
+write_feather(df, "data/na_summary_cleaned.feather")
